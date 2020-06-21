db17f32be0501d2cc42a84a5071883bff2b3e807
AliciaMstt <ticatla@gmail.com>
2020-06-21 17:12:23 -0500

    make pseudo legend look nicer in final plot grid

--- bin/2.PlotRasters.R
+++ bin/2.PlotRasters.R
@@ -4,7 +4,7 @@ library(raster)
 
 ########### plots Resistance rasters #########
 
-par(mfrow=c(10,3)) #the number of rows and columns the figure would have
+par(mfrow=c(8,4)) #the number of rows and columns the figure would have
 
 # use different grey colors depending on values for conduductaces for altitude A and B
 # 0 = white
@@ -14,6 +14,7 @@ par(mfrow=c(10,3)) #the number of rows and columns the figure would have
 # .5 = "grey50"
 # .7 = "grey30"
 # .9 = "grey10"
+# 1 ="black·
 
 ## Elevation
 # crude altitdes conductances .2, 1
@@ -132,13 +133,14 @@ plot(x, col=c("grey"),  legend=FALSE, xaxt='n', yaxt='n')
 points(Ppoints[,5], Ppoints[,6], col="darkgreen", cex=0.3)
 
 ## pseudo legend
-x<- rep(1,8)
-y<- seq(.1,8)
-plot(x,y, pch=22, cex=8, main= "Conductance",
-     bg= c("white", "grey90", "grey80", "grey70", "grey50", "grey30", "grey10", "black"),
-     xaxt='n', yaxt='n', xlab='', ylab='', bty='n')
-text(x=1.1, y=y, cex=2,
-     labels=c("0", "0.1", "0.2", "0.3", "0.5", "0.7", "0.9", "1"))
-
+x<- c(rep(1,4), rep(1.7,4))
+y<- c(seq(.1,4), seq(.1,4))
+plot(x,y, pch=22, cex=3,
+     bg= c("grey70","grey80", "grey90", "white", "black", "grey10", "grey30", "grey50"),
+     xaxt='n', yaxt='n', xlab='', ylab='', bty='n', # delete axis and box stuff
+     xlim=c(-.1,3), ylim=c(-2,6)) # this makes points to be closer to each other
+text(x=x+.3, y=y, cex=1.2,
+     labels=c("0.3", "0.2", "0.1", "0", "1", "0.9", "0.7", "0.5"))
+text(x=.8, y=4.5, labels="Conductances", adj=0, cex=1.5)
 
 

d0a7fcdfb8178803bcad8d6bd1a06e707b23c2db
Nancy-Galvez <nancygalvezr@gmail.com>
2020-10-12 18:02:39 -0500

    I changed sourse for source in the script

--- bin/4.to_get_Diversity_All.r
+++ bin/4.to_get_Diversity_All.r
@@ -20,49 +20,49 @@ library(lattice)
 
 
 #'**We get the diversity analysis at haplotypes, CL 3%, and CL 5% in Diptera**'#
-# Sourse to scripts Diptera and data
+# Source to scripts Diptera and data
 source("Diptera/Com_matrixes_data_exploration_Diptera_h_Site.R") 
 source("Diptera/Com_matrixes_data_exploration_Diptera_3P_Site.R") 
 source("Diptera/Com_matrixes_data_exploration_Diptera_5P_Site.R") 
 
 #'**We get the diversity analysis at haplotypes, CL 3%, and CL 5% in Collembola**'#
-# Sourse to scripts Collembola and data
+# Source to scripts Collembola and data
 source("Collembola/Com_matrixes_data_exploration_Collembola_h_Site.R") 
 source("Collembola/Com_matrixes_data_exploration_Collembola_3P_Site.R") 
 source("Collembola/Com_matrixes_data_exploration_Collembola_5P_Site.R") 
 
 #'**We get the diversity analysis at haplotypes, CL 3%, and CL 5% in Arachnida**'#
-# Sourse to scripts Arachnida and data
+# Source to scripts Arachnida and data
 source("Arachnida/Com_matrixes_data_exploration_Arachnida_h_Site.R") 
 source("Arachnida/Com_matrixes_data_exploration_Arachnida_3P_Site.R") 
 source("Arachnida/Com_matrixes_data_exploration_Arachnida_5P_Site.R")
 
 #'**We get the diversity analysis at haplotypes, CL 3%, and CL 5% in Hemiptera**'#
-# Sourse to scripts Hemiptera and data
+# Source to scripts Hemiptera and data
 source("Hemiptera/Com_matrixes_data_exploration_Hemiptera_h_Site.R") 
 source("Hemiptera/Com_matrixes_data_exploration_Hemiptera_3P_Site.R") 
 source("Hemiptera/Com_matrixes_data_exploration_Hemiptera_5P_Site.R") 
 
 #'**We get the diversity analysis at haplotypes, CL 3%, and CL 5% in Hymenoptera**'#
-# Sourse to scripts Hymenoptera and data
+# Source to scripts Hymenoptera and data
 source("Hymenoptera/Com_matrixes_data_exploration_Hymenoptera_h_Site.R") 
 source("Hymenoptera/Com_matrixes_data_exploration_Hymenoptera_3P_Site.R") 
 source("Hymenoptera/Com_matrixes_data_exploration_Hymenoptera_5P_Site.R") 
 
 #'**We get the diversity analysis at haplotypes, CL 3%, and CL 5% in Coleoptera**'#
-# Sourse to scripts Coleoptera and data
+# Source to scripts Coleoptera and data
 source("Coleoptera/Com_matrixes_data_exploration_Coleoptera_h_Site.R") 
 source("Coleoptera/Com_matrixes_data_exploration_Coleoptera_3P_Site.R") 
 source("Coleoptera/Com_matrixes_data_exploration_Coleoptera_5P_Site.R") 
 
 #'**We get the diversity analysis at haplotypes, CL 3%, and CL 5% in Myriapoda**'#
-# Sourse to scripts Myriapoda and data
+# Source to scripts Myriapoda and data
 source("Myriapoda/Com_matrixes_data_exploration_Myriapoda_h_Site.R") 
 source("Myriapoda/Com_matrixes_data_exploration_Myriapoda_3P_Site.R") 
 source("Myriapoda/Com_matrixes_data_exploration_Myriapoda_5P_Site.R")
 
 #'**We get the diversity analysis at haplotypes, CL 3%, and CL 5% in Lepidoptera**'#
-# Sourse to scripts Lepidoptera and data
+# Source to scripts Lepidoptera and data
 source("Lepidoptera/Com_matrixes_data_exploration_Lepidoptera_h_Site.R") 
 source("Lepidoptera/Com_matrixes_data_exploration_Lepidoptera_3P_Site.R") 
 source("Lepidoptera/Com_matrixes_data_exploration_Lepidoptera_5P_Site.R") 

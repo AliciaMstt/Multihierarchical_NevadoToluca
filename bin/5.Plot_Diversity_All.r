1c0e2fbefc68ef8da70a4dfb0e384d9fb5cd0de9
Nancy-Galvez <nancygalvezr@gmail.com>
2020-11-13 12:41:27 -0600

    I modified script 5.Plot_Diversity_All

--- bin/5.Plot_Diversity_All.r
+++ bin/5.Plot_Diversity_All.r
@@ -15,11 +15,9 @@ library(easypackages)
 ################ plots Community diversity and composition #####################
 
 
-png(filename="../figures/RelativeSpeciesRichness1.png", width=920 , height=1536, units="px") # set size of the file to plot 
-par(mfrow=c(8,3), xpd = TRUE) #the number of rows and columns the figure would have
+png(filename="../figures/RelativeSpeciesRichness.png", width=920 , height=1536, units="px") # set size of the file to plot 
+par(mfrow=c(8,3), mar = c(3, 2.5, 2, 2), omi=c(0.5, 0.5, 0.5, 0.5)) #the number of rows and columns the figure would have
 
-
-#mar = c(2.5, 3, 3, 3), xpd = TRUE),
 #'**Creating plot'**#
 
 #'**Community diversity and composition at the haplotype, 3% and 5% Clustering levels for each of the eight taxonomic orders studied**'#
@@ -37,9 +35,9 @@ kruskal.test(sample_richness_Site_h ~ Mountain1Site, data = richness_Site_h_Dipt
 posthoc.kruskal.nemenyi.test(x=richness_Site_h_Diptera$sample_richness_Site_h, g=richness_Site_h_Diptera$Mountain1Site, method="Bonferroni")
 # Comparison of each group against. 
 axis(1, at=NULL, label=F, tick=TRUE, lwd=1.3)
-text(x=c(1,2,3,4.1), y=(152), labels=c("a","a","ab","b"), cex=1.5)
+text(x=c(1,2,3,4.1), y=(152), labels=c("a","a","ab","b"), cex=2)
 text(x=4.5, y=152, labels="*", cex=2)
-mtext(c("Haplotypes"), side = 3, col = "black", line = 1, cex = 1.5)
+mtext(c("Haplotypes"), side = 3, col = "black", line = 1, cex=2)
 
 read.table("../genetic/Data_out/Diptera/Diptera3P/richness_Site0.03_Diptera.txt",header=TRUE)->richness_Site0.03_Diptera
 
@@ -48,9 +46,9 @@ kruskal.test(sample_richness_Site0.03 ~ Mountain1Site, data = richness_Site0.03_
 posthoc.kruskal.nemenyi.test(x=richness_Site0.03_Diptera$sample_richness_Site0.03, g=richness_Site0.03_Diptera$Mountain1Site, method="Bonferroni")
 # Comparison of each group against.
 axis(1, at=NULL, label=F, tick=TRUE, lwd=1.3)
-text(x=c(1,2,3,4.1), y=(48), labels=c("ab","a","ab","b"), cex=1.5)
+text(x=c(1,2,3,4.1), y=(48), labels=c("ab","a","ab","b"), cex=2)
 text(x=4.5, y=48, labels="*", cex=2)
-mtext(c("Clustering 3%"), side = 3, col = "black", line = 1, cex = 1.5)
+mtext(c("Clustering 3%"), side = 3, col = "black", line = 1, cex=2)
 
 read.table("../genetic/Data_out/Diptera/Diptera5P/richness_Site0.05_Diptera.txt",header=TRUE)->richness_Site0.05_Diptera
 
@@ -59,10 +57,10 @@ kruskal.test(sample_richness_Site0.05 ~ Mountain1Site, data = richness_Site0.05_
 posthoc.kruskal.nemenyi.test(x=richness_Site0.05_Diptera$sample_richness_Site0.05, g=richness_Site0.05_Diptera$Mountain1Site, method="Bonferroni")
 # Comparison of each group against.
 axis(1, at=NULL, label=F, tick=TRUE, lwd=1.3)
-text(x=c(1,2,3,4.1), y=(48), labels=c("ab","a","ab","b"), cex=1.5)
+text(x=c(1,2,3,4.1), y=(48), labels=c("ab","a","ab","b"), cex=2)
 text(x=4.5, y=48, labels="*", cex=2)
-mtext(c("Clustering 5%"), side = 3, col = "black", line = 1, cex = 1.5)
-mtext(c("Diptera"), side = 4, col = "black", line = 1, cex = 1.5)
+mtext(c("Clustering 5%"), side = 3, col = "black", line = 1, cex=2)
+mtext(c("Diptera"), side = 4, col = "black", line = 1, cex=2)
 
 
 #'**Collembola'**#
@@ -84,7 +82,7 @@ kruskal.test(sample_richness_Site0.03 ~ Mountain1Site, data = richness_Site0.03_
 posthoc.kruskal.nemenyi.test(x=richness_Site0.03_Collembola$sample_richness_Site0.03, g=richness_Site0.03_Collembola$Mountain1Site, method="Bonferroni")
 # Comparison of each group against.
 axis(1, at=NULL, label=F, tick=TRUE, lwd=1.3)
-text(x=c(1,2,3,4), y=(24), labels=c("a","a","a","b"), cex=1.5)
+text(x=c(1,2,3,4), y=(24), labels=c("a","a","a","b"), cex=2)
 text(x=4.5, y=24, labels="*", cex=2)
 
 read.table("../genetic/Data_out/Collembola/Collembola5P/richness_Site_Collembola0.05.txt",header=TRUE)->richness_Site0.05_Collembola
@@ -96,7 +94,7 @@ posthoc.kruskal.nemenyi.test(x=richness_Site0.05_Collembola$sample_richness_Site
 #text(x=c(1,2,3,4), y=(24), labels=c("a","a","a","b"), cex=1.4)
 axis(1, at=NULL, label=F, tick=TRUE, lwd=1.3)
 text(x=4.5, y=24, labels="ns", cex=2)
-mtext(c("Collembola"), side = 4, col = "black", line = 1, cex = 1.5)
+mtext(c("Collembola"), side = 4, col = "black", line = 1, cex=2)
 
 #'**Arachnida'**#
 
@@ -107,7 +105,7 @@ kruskal.test(sample_richness_Site_h ~ Mountain1Site, data = richness_Site_h_Arac
 posthoc.kruskal.nemenyi.test(x=richness_Site_h_Arachnida$sample_richness_Site_h, g=richness_Site_h_Arachnida$Mountain1Site, method="Bonferroni")
 # Comparison of each group against.
 axis(1, at=NULL, label=F, tick=TRUE, lwd=1.3)
-text(x=c(1,2,3,4), y=(29), labels=c("a","a","b","b"), cex=1.5)
+text(x=c(1,2,3,4), y=(29), labels=c("a","a","b","b"), cex=2)
 text(x=4.5, y=29, labels="***", cex=2)
 
 read.table("../genetic/Data_out/Arachnida/Arachnida3P/richness_Site_Arachnida_0.03.txt",header=TRUE)->richness_Site0.03_Arachnida
@@ -117,7 +115,7 @@ kruskal.test(sample_richness_Site0.03 ~ Mountain1Site, data = richness_Site0.03_
 posthoc.kruskal.nemenyi.test(x=richness_Site0.03_Arachnida$sample_richness_Site0.03, g=richness_Site0.03_Arachnida$Mountain1Site, method="Bonferroni")
 # Comparison of each group against.
 axis(1, at=NULL, label=F, tick=TRUE, lwd=1.3)
-text(x=c(1,2,3,4), y=(29), labels=c("a","a","b","b"), cex=1.5)
+text(x=c(1,2,3,4), y=(29), labels=c("a","a","b","b"), cex=2)
 text(x=4.5, y=29, labels="***", cex=2)
 
 read.table("../genetic/Data_out/Arachnida/Arachnida5P/richness_Site_Arachnida0.05.txt",header=TRUE)->richness_Site0.05_Arachnida
@@ -127,9 +125,9 @@ kruskal.test(sample_richness_Site0.05 ~ Mountain1Site, data = richness_Site0.05_
 posthoc.kruskal.nemenyi.test(x=richness_Site0.05_Arachnida$sample_richness_Site0.05, g=richness_Site0.05_Arachnida$Mountain1Site, method="Bonferroni")
 # Comparison of each group against.
 axis(1, at=NULL, label=F, tick=TRUE, lwd=1.3)
-text(x=c(1,2,3,4), y=(29), labels=c("a","a","b","b"), cex=1.5)
+text(x=c(1,2,3,4), y=(29), labels=c("a","a","b","b"), cex=2)
 text(x=4.5, y=29, labels="***", cex=2)
-mtext(c("Arachnida"), side = 4, col = "black", line = 1, cex = 1.5)
+mtext(c("Arachnida"), side = 4, col = "black", line = 1, cex=2)
 
 
 #'**Hempitera'**#
@@ -163,7 +161,7 @@ posthoc.kruskal.nemenyi.test(x=richness_Site0.05$sample_richness_Site0.05, g=ric
 #text(x=c(1,2,3,4.1), y=(153), labels=c("a","a","ab","b"), cex=1.4)
 axis(1, at=NULL, label=F, tick=TRUE, lwd=1.3)
 text(x=4.5, y=14, labels="ns", cex=2)
-mtext(c("Hemiptera"), side = 4, col = "black", line = 1, cex = 1.5)
+mtext(c("Hemiptera"), side = 4, col = "black", line = 1, cex=2)
 
 
 #'**Hymenoptera'**#
@@ -175,7 +173,7 @@ kruskal.test(sample_richness_Site_h ~ Mountain1Site, data = richness_Site_h)
 posthoc.kruskal.nemenyi.test(x=richness_Site_h$sample_richness_Site_h, g=richness_Site_h$Mountain1Site, method="Bonferroni")
 # Comparison of each group against.
 axis(1, at=NULL, label=F, tick=TRUE, lwd=1.3)
-text(x=c(1,2,3,4.1), y=(29), labels=c("a","b","a","a"), cex=1.5)
+text(x=c(1,2,3,4.1), y=(29), labels=c("a","b","a","a"), cex=2)
 text(x=4.5, y=29, labels="**", cex=2)
 
 read.table("../genetic/Data_out/Hymenoptera/Hymenoptera3P/richness_Site0.03_Hymenoptera.txt",header=TRUE)->richness_Site0.03
@@ -185,7 +183,7 @@ kruskal.test(sample_richness_Site0.03 ~ Mountain1Site, data = richness_Site0.03)
 posthoc.kruskal.nemenyi.test(x=richness_Site0.03$sample_richness_Site0.03, g=richness_Site0.03$Mountain1Site, method="Bonferroni")
 # Comparison of each group against.
 axis(1, at=NULL, label=F, tick=TRUE, lwd=1.3)
-text(x=c(1,2,3,4.1), y=(29), labels=c("a","b","a","a"), cex=1.5)
+text(x=c(1,2,3,4.1), y=(29), labels=c("a","b","a","a"), cex=2)
 text(x=4.5, y=29, labels="*", cex=2)
 
 read.table("../genetic/Data_out/Hymenoptera/Hymenoptera5P/richness_Site0.05_Hymenoptera.txt",header=TRUE)->richness_Site0.05
@@ -197,7 +195,7 @@ posthoc.kruskal.nemenyi.test(x=richness_Site0.05$sample_richness_Site0.05, g=ric
 #text(x=c(1,2,3,4.1), y=(29), labels=c("a","b","a","a"), cex=1.4)
 axis(1, at=NULL, label=F, tick=TRUE, lwd=1.3)
 text(x=4.5, y=29, labels="ns", cex=2)
-mtext(c("Hymenoptera"), side = 4, col = "black", line = 1, cex = 1.5)
+mtext(c("Hymenoptera"), side = 4, col = "black", line = 1, cex=2)
 
 
 #'**Coleoptera'**#
@@ -209,7 +207,7 @@ kruskal.test(sample_richness_Site_h ~ Mountain1Site, data = richness_Site_h)
 posthoc.kruskal.nemenyi.test(x=richness_Site_h$sample_richness_Site_h, g=richness_Site_h$Mountain1Site, method="Bonferroni")
 # Comparison of each group against.
 axis(1, at=NULL, label=F, tick=TRUE, lwd=1.3)
-text(x=c(1,2,3,4), y=(15), labels=c("a","b","ab","b"), cex=1.5)
+text(x=c(1,2,3,4), y=(15), labels=c("a","b","ab","b"), cex=2)
 text(x=4.5, y=15, labels="**", cex=2)
 
 read.table("../genetic/Data_out/Coleoptera/Coleoptera3P/richness_Site_Coleoptera0.03.txt",header=TRUE)->richness_Site0.03
@@ -219,7 +217,7 @@ kruskal.test(sample_richness_Site0.03 ~ Mountain1Site, data = richness_Site0.03)
 posthoc.kruskal.nemenyi.test(x=richness_Site0.03$sample_richness_Site0.03, g=richness_Site0.03$Mountain1Site, method="Bonferroni")
 # Comparison of each group against.
 axis(1, at=NULL, label=F, tick=TRUE, lwd=1.3)
-text(x=c(1,2,3,4), y=(15), labels=c("a","b","ab","b"), cex=1.5)
+text(x=c(1,2,3,4), y=(15), labels=c("a","b","ab","b"), cex=2)
 text(x=4.5, y=15, labels="**", cex=2)
 
 read.table("../genetic/Data_out/Coleoptera/Coleoptera5P/richness_Site0.05_Coleoptera.txt",header=TRUE)->richness_Site0.05
@@ -229,9 +227,9 @@ kruskal.test(sample_richness_Site0.05 ~ Mountain1Site, data = richness_Site0.05)
 posthoc.kruskal.nemenyi.test(x=richness_Site0.05$sample_richness_Site0.05, g=richness_Site0.05$Mountain1Site, method="Bonferroni")
 # Comparison of each group against. 
 axis(1, at=NULL, label=F, tick=TRUE, lwd=1.3)
-text(x=c(1,2,3,4), y=(15), labels=c("a","b","ab","b"), cex=1.5)
+text(x=c(1,2,3,4), y=(15), labels=c("a","b","ab","b"), cex=2)
 text(x=4.5, y=15, labels="**", cex=2)
-mtext(c("Coleoptera"), side = 4, col = "black", line = 1, cex = 1.5)
+mtext(c("Coleoptera"), side = 4, col = "black", line = 1, cex=2)
 
 #'**Myriapoda'**#
 
@@ -264,7 +262,7 @@ posthoc.kruskal.nemenyi.test(x=richness_Site0.05$sample_richness_Site0.05, g=ric
 #text(x=c(1,2,3,4.1), y=(29), labels=c("a","b","a","a"), cex=1.4)
 axis(1, at=NULL, label=F, tick=TRUE, lwd=1.3)
 text(x=4.5, y=7, labels="ns", cex=2)
-mtext(c("Myriapoda"), side = 4, col = "black", line = 1, cex = 1.5)
+mtext(c("Myriapoda"), side = 4, col = "black", line = 1, cex=2)
 
 #'**Lepidoptera'**#
 
@@ -301,10 +299,11 @@ posthoc.kruskal.nemenyi.test(x=richness_Site0.05$sample_richness_Site0.05, g=ric
 axis(1, at=NULL, label=F, tick=TRUE, lwd=1.3)
 text(x=c(1,2,3,4), -1.3, labels=c("AAB","ASB","SJH","TLC"), cex=2, xpd=NA)
 text(x=4.5, y=7, labels="ns", cex=2)
-mtext(c("Lepidoptera"), side = 4, col = "black", line = 1, cex = 1.5)
+mtext(c("Lepidoptera"), side = 4, col = "black", line = 1, cex=2)
 
-mtext("S a m p l e d   s i t e s", side=1, outer=TRUE, line=-1, cex=2)
-mtext("R i c h n e s s", side=2, outer=TRUE, line=-2, cex=2) 
+mtext("S a m p l e d   s i t e s", side=1, outer=TRUE, line=1.5, cex=2.5)
+mtext("R i c h n e s s", side=2, outer=TRUE, line=0.7, cex=2.5) 
+
 
 dev.off()
 

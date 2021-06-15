1952a644b5ee9f642fce9e52ce93b858477ec06a
Nancy-Galvez <nancygalvezr@gmail.com>
2021-06-15 01:22:07 -0500

    I added install software, Steps after processing, Codon Stops, get AVS tables

---
+++ bin/0.4to_get_ASV_tables.sh
@@ -0,0 +1,37 @@
+#SBATCH --mem=64000
+#SBATCH -n 10
+
+#1Diptera2059Reads_realigned.fasta
+
+vsearch --search_exact LENGTH420CatTratamientos137Sopas.Ptrim.trimmo.Maxee1.LABEL.sorted420NOPuntoComa.fas --db 1Diptera2059Reads_realigned.fasta --otutabout allele_table_1Diptera2059Reads_realigned.txt
+
+
+#2Collembola1220Reads_realigned.fasta
+
+vsearch --search_exact LENGTH420CatTratamientos137Sopas.Ptrim.trimmo.Maxee1.LABEL.sorted420NOPuntoComa.fas --db 2Collembola1220Reads_realigned.fasta --otutabout allele_table_2Collembola1220Reads_realigned.txt 
+
+#3Arachnida428Reads_realigned.fasta
+
+vsearch --search_exact LENGTH420CatTratamientos137Sopas.Ptrim.trimmo.Maxee1.LABEL.sorted420NOPuntoComa.fas --db 3Arachnida428Reads_realigned.fasta --otutabout allele_table_3Arachnida428Reads_realigned.txt
+
+#4Hymenoptera304Reads_realigned.fasta
+
+vsearch --search_exact LENGTH420CatTratamientos137Sopas.Ptrim.trimmo.Maxee1.LABEL.sorted420NOPuntoComa.fas --db 4Hymenoptera304Reads_realigned.fasta --otutabout allele_table_4Hymenoptera304Reads_realigned.txt
+
+
+#5Coleoptera227Reads_realigned.fasta
+
+vsearch --search_exact LENGTH420CatTratamientos137Sopas.Ptrim.trimmo.Maxee1.LABEL.sorted420NOPuntoComa.fas --db 5Coleoptera227Reads_realigned.fasta --otutabout allele_table_5Coleoptera227Reads_realigned.txt 
+
+
+#6Paraneoptera210Reads_realigned.fasta
+
+vsearch --search_exact LENGTH420CatTratamientos137Sopas.Ptrim.trimmo.Maxee1.LABEL.sorted420NOPuntoComa.fas --db 6Paraneoptera210Reads_realigned.fasta --otutabout allele_table_6Paraneoptera210Reads_realigned.txt 
+
+#7Lepidoptera59Reads_realigned.fasta
+
+vsearch --search_exact LENGTH420CatTratamientos137Sopas.Ptrim.trimmo.Maxee1.LABEL.sorted420NOPuntoComa.fas --db 7Lepidoptera59Reads_realigned.fasta --otutabout allele_table_7Lepidoptera59Reads_realigned.txt 
+
+#8Myriapoda57Reads_realigned.fasta
+
+vsearch --search_exact LENGTH420CatTratamientos137Sopas.Ptrim.trimmo.Maxee1.LABEL.sorted420NOPuntoComa.fas --db 8Myriapoda57Reads_realigned.fasta --otutabout allele_table_8Myriapoda57Reads_realigned.txt
